b01f491e6e6f6a0ff7a06a3f18966a4fe6c3d0eb
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-10 21:38:47 +0000

    Add failing unit tests to highlight that the piece does not currently take direction into account when moving

--- NVMGame/CombinedCommandUnitTests.cs
+++ NVMGame/CombinedCommandUnitTests.cs
@@ -13,5 +13,32 @@ namespace NVMGame
 			piece.Move("MMMM");
 			Assert.AreEqual("0 4 N", piece.CurrentPosition);
 		}
+
+		[Test]
+		public void When_A_New_Piece_Is_Created_On_A_5_By_5_Board_And_Given_The_Command_RM_Its_Final_Position_Should_Be_1_0_E()
+		{
+			var piece = new Piece(new Board(5, 5));
+
+			piece.Move("RM");
+			Assert.AreEqual("1 0 E", piece.CurrentPosition);
+		}
+
+		[Test]
+		public void When_A_New_Piece_Is_Created_On_A_5_By_5_Board_And_Given_The_Command_RMLLM_Its_Final_Position_Should_Be_0_0_W()
+		{
+			var piece = new Piece(new Board(5, 5));
+
+			piece.Move("RMLLM");
+			Assert.AreEqual("0 0 W", piece.CurrentPosition);
+		}
+
+		[Test]
+		public void When_A_New_Piece_Is_Created_On_A_5_By_5_Board_And_Given_The_Command_MRRM_Its_Final_Position_Should_Be_0_0_S()
+		{
+			var piece = new Piece(new Board(5, 5));
+
+			piece.Move("MRRM");
+			Assert.AreEqual("0 0 S", piece.CurrentPosition);
+		}
     }
 }
