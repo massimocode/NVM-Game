0433548ce1cd86b2989e84e5ac6dbf027b208e69
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-10 21:31:55 +0000

    Add additional unit tests to assert successful 360-degree rotation in both directions

--- NVMGame/UnitTests.cs
+++ NVMGame/UnitTests.cs
@@ -42,5 +42,41 @@ namespace NVMGame
 
 			Assert.AreEqual("0 0 W", piece.CurrentPosition);
 		}
+
+		[Test]
+		public void When_A_New_Piece_Is_Created_On_A_5_By_5_Board_And_Given_The_Command_R_Four_Times_Its_Final_Position_Should_Be_0_0_N()
+		{
+			var piece = new Piece(new Board(5, 5));
+
+			piece.Move("R");
+			Assert.AreEqual("0 0 E", piece.CurrentPosition);
+
+			piece.Move("R");
+			Assert.AreEqual("0 0 S", piece.CurrentPosition);
+
+			piece.Move("R");
+			Assert.AreEqual("0 0 W", piece.CurrentPosition);
+
+			piece.Move("R");
+			Assert.AreEqual("0 0 N", piece.CurrentPosition);
+		}
+
+		[Test]
+		public void When_A_New_Piece_Is_Created_On_A_5_By_5_Board_And_Given_The_Command_L_Four_Times_Its_Final_Position_Should_Be_0_0_N()
+		{
+			var piece = new Piece(new Board(5, 5));
+
+			piece.Move("L");
+			Assert.AreEqual("0 0 W", piece.CurrentPosition);
+
+			piece.Move("L");
+			Assert.AreEqual("0 0 S", piece.CurrentPosition);
+
+			piece.Move("L");
+			Assert.AreEqual("0 0 E", piece.CurrentPosition);
+
+			piece.Move("L");
+			Assert.AreEqual("0 0 N", piece.CurrentPosition);
+		}
     }
 }
